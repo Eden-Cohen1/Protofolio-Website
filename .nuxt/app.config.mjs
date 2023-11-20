2358d2bb47ea028997dbea0986dc8e394f743f05
Eden-Cohen1 <edencohen.dev@gmail.com>
2023-11-20 17:43:35 +0200

    b

--- .nuxt/app.config.mjs
+++ .nuxt/app.config.mjs
@@ -4,7 +4,7 @@ import { defuFn } from 'defu'
 
 const inlineConfig = {
   "nuxt": {
-    "buildId": "dev"
+    "buildId": "fded00fd-bd38-4d51-90d1-ca392ece569f"
   }
 }
 
--- .nuxt/types/app.config.d.ts
+++ .nuxt/types/app.config.d.ts
@@ -5,7 +5,7 @@ import type { Defu } from 'defu'
 
 declare const inlineConfig = {
   "nuxt": {
-    "buildId": "dev"
+    "buildId": "fded00fd-bd38-4d51-90d1-ca392ece569f"
   }
 }
 type ResolvedAppConfig = Defu<typeof inlineConfig, []>
