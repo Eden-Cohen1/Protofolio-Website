c8d19c9ae8a5939ec48bfd82e4b0342d543f1b42
Eden-Cohen1 <edencohen.dev@gmail.com>
2023-11-20 18:11:57 +0200

    b

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 
 export default defineNuxtConfig({
+  routeRules: {
+    "/": { prerender: true },
+  },
   devtools: { enabled: true },
   modules: [
     "nuxt-icon",
